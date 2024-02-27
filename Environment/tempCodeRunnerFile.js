908fd07b173887cc316744cc9b51962530aa19e8
Mayank Meghwal <125905142+itz-Mayank@users.noreply.github.com>
2024-02-27 21:37:01 +0530

    Add files via upload

---
+++ Environment/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+ if(url=='./file 10.html'){
+    //         res.end(Home);
+    //     }
+        
+    //     else if(url=='file 3.html'){
+    //         res.end(about);
+    //     }
+        
+    // else if(url=='file 8.html'){
+    //     res.end(services);
+    // }
+    
+    // else if(url=='file 4.html'){
+    //     res.end(contact);
+    // }
+    
+    // else {
+    //     res.statusCode = 404;
+    //     res.end("<h1> 404 not found")
+    // }
